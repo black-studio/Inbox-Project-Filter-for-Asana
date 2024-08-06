1d20b9573d145af3e28ccafbec8430fb24956936
Francesco Canovi <francesco@blackstudio.it>
2024-08-06 16:44:02 +0200

    Create background.js
    First commit

---
+++ background.js
@@ -0,0 +1,8 @@
+chrome.webNavigation.onCompleted.addListener(() => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      files: ["content.js"]
+    });
+  });
+}, { url: [{ urlMatches: 'https://app.asana.com/*' }] });
